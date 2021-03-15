6a756fd5475cdcca097cdbc4b3f3d3dbbcdbe889
Johannes Nilsson <johannes@nilsson.im>
2021-03-15 05:22:29 +0100

    Add login form

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/* index.php
+ * The index of the CMS. Let's the user log in.
+ * joni1307@student.miun.se | HT20 | DT173G, Projekt
+ */
+
+?>
+<h1>Portfolio</h1>
+
+<div class="form-container">
+   <?php if(isset($message)){ echo '<p class="' . $message["type"] . '">' . $message["text"] . '</p>'; } ?> 
+
+   <form action="POST">
+      <label for="email">E-post</label>
+      <input type="email" name="email" id="email">
+      <label for="password">Lösenord</label>
+      <input type="password" name="password" id="password">
+      <input type="submit" value="Logga in">
+   </form>
+   <p class="small">Har du inget konto? <a href="register.php">Registrera dig här</a>.</p>
+</div>
