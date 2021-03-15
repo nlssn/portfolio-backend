2e8474561efebadff58f95b0744797c97698004c
Johannes Nilsson <johannes@nilsson.im>
2021-03-15 05:53:21 +0100

    Add placeholder content

---
+++ admin.php
@@ -0,0 +1,15 @@
+<?php
+
+/* admin.php
+ * The dashboard of the CMS. Where the user can access their content.
+ * joni1307@student.miun.se | HT20 | DT173G, Projekt
+ */
+$page_title = "Adminpanel";
+require_once("includes/layout/header.php");
+?>
+
+<h1>Du är nu inloggad!</h1>
+<p>Nedan kan du se lite information om det material du skapat.</p>
+
+<?php
+require_once("includes/layout/footer.php");
