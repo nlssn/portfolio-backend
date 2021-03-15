aaf43afb360cc577b1ce94cfe4a98e12fa5edc7d
Johannes Nilsson <johannes@nilsson.im>
2021-03-15 05:29:30 +0100

    Add header + nav HTML

---
+++ includes/layout/header.php
@@ -0,0 +1,27 @@
+<?php
+/* includes/layout/header.php
+ * It's the header.
+ * joni1307@student.miun.se | HT20 | DT173G, Projekt
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body class="<?= basename($_SERVER['SCRIPT_FILENAME'], '.php') ?>">
+
+<header class="site-header">
+   <p class="logo">Portfolio</p>
+
+   <nav class="nav-primary">
+      <ul>
+         <li><a href="#0">Adminpanel</a></li>
+         <li><a href="#0">Skapa</a></li>
+         <li><a href="#0">Logga ut</a></li>
+      </ul>
+   </nav>
+</header>
