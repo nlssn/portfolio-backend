bbb9ccb45e40917ffdef31438b3649a57961e346
Johannes Nilsson <johannes@nilsson.im>
2021-03-15 06:07:03 +0100

    Add handling of messages

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
  * The index of the CMS. Let's the user log in.
  * joni1307@student.miun.se | HT20 | DT173G, Projekt
  */
+
+// Handle any incoming message
+if(isset($_GET["msg"]) && isset($_GET["type"])) {
+   $msg = array(
+      "text" => $_GET["msg"],
+      "type" => $_GET["type"]
+   );
+}
+
 $page_title = "Logga in";
 require_once("includes/layout/header.php");
 ?>
@@ -11,7 +20,7 @@ require_once("includes/layout/header.php");
 <h1><?= SITENAME ?></h1>
 
 <div class="form-container">
-   <?php if(isset($message)){ echo '<p class="' . $message["type"] . '">' . $message["text"] . '</p>'; } ?> 
+   <?php if(isset($msg)){ echo '<p class="' . $msg["type"] . '">' . $msg["text"] . '</p>'; } ?> 
 
    <form action="POST">
       <label for="email">E-post</label>
