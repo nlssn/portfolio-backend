93f35465bf0f71171c8ca83b982acbaf59a3a424
Johannes Nilsson <johannes@nilsson.im>
2021-03-15 05:39:58 +0100

    Add footer

---
+++ includes/layout/footer.php
@@ -0,0 +1,12 @@
+<?php
+/* includes/layout/footer.php
+ * It's the footer.
+ * joni1307@student.miun.se | HT20 | DT173G, Projekt
+ */
+?>
+
+   <footer class="site-footer">
+      <p>&copy; <?= date("Y") ?> &middot; Johannes Nilsson</p>
+   </footer>
+</body>
+</html>
