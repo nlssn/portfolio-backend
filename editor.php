b462ad471565b0191aa15cb104a8895b63e368c5
Johannes Nilsson <johannes@nilsson.im>
2021-03-17 09:07:00 +0100

    Add dynamic page titles

--- editor.php
+++ editor.php
@@ -12,11 +12,34 @@ if(!isset($_SESSION["id"])) {
    header("Location: index.php?msg=Du måste logga in&type=error");
 }
 
-$page_title = "Skapa";
+// Check if we should be editing or creating something new
+if(!isset($_GET["id"])) {
+   $editing = false;
+   $title_prefix = "Lägg till";
+} else {
+   $editing = true;
+   $title_prefix = "Redigera";
+}
+
+// Check what type of content we're editing
+if(!isset($_GET["contentType"])) {
+   header("Location: admin.php");
+} else {
+   switch ($_GET["contentType"]) {
+      case "experience":
+         $contentType = "experience";
+         $page_title = $editing ? $title_prefix . " erfarenhet" : $title_prefix . " ny erfarenhet";
+         break;
+      case "portfolio":
+         $contentType = "portfolio";
+         $page_title = $editing ? $title_prefix . " portfolioprojekt" : $title_prefix . " nytt portfolioprojekt";;
+   }
+}
+
 require_once("includes/layout/header.php");
 ?>
 
-<h1>Skapa {ny erfarenhet / portfolioinlägg}</h1>
+<h1><?= $page_title ?></h1>
 <p>Här laddar vi ett formulär beroende på vad du vill redigera</p>
 
 <?php
