3824f4d9f48a1330e4f512f2a481cf45e919eeae
Johannes Nilsson <johannes@nilsson.im>
2021-03-17 13:09:22 +0100

    Display forms depending on URL params

--- editor.php
+++ editor.php
@@ -1,7 +1,7 @@
 <?php
 /* editor.php
  * The content editor for the CMS. 
- * Loads forms depending on which content type the user want's to edit.
+ * Loads forms depending on which content type the user want's to edit or create.
  * joni1307@student.miun.se | HT20 | DT173G, Projekt
  */
 
@@ -12,35 +12,53 @@ if(!isset($_SESSION["id"])) {
    header("Location: index.php?msg=Du måste logga in&type=error");
 }
 
-// Check if we should be editing or creating something new
-if(!isset($_GET["id"])) {
-   $editing = false;
-   $title_prefix = "Lägg till";
-} else {
-   $editing = true;
-   $title_prefix = "Redigera";
-}
-
-// Check what type of content we're editing
+// Check what type of content we're trying to edit/create
 if(!isset($_GET["contentType"])) {
    header("Location: admin.php");
 } else {
-   switch ($_GET["contentType"]) {
-      case "experience":
-         $contentType = "experience";
-         $page_title = $editing ? $title_prefix . " erfarenhet" : $title_prefix . " ny erfarenhet";
-         break;
-      case "portfolio":
-         $contentType = "portfolio";
-         $page_title = $editing ? $title_prefix . " portfolioprojekt" : $title_prefix . " nytt portfolioprojekt";;
-   }
+   $contentType = $_GET["contentType"];
+}
+
+// Check if we should be editing or creating something new
+if(!isset($_GET["id"])) {
+   $isEditing = false;
+   $title_prefix = "Lägg till";
+} else {
+   $isEditing = true;
+   $id = $_GET["id"];
+   $title_prefix = "Redigera";
+}
+
+// Set the page_title depending on contentType and isEditing
+switch ($contentType) {
+   case "employment":
+      $page_title = $isEditing ? $title_prefix . " arbetslivserfarenhet" : $title_prefix . " ny arbetslivserfarenhet";
+      break;
+   case "education":
+      $page_title = $isEditing ? $title_prefix . " utbildning" : $title_prefix . " ny utbildning";
+      break;
+   case "portfolio":
+      $page_title = $isEditing ? $title_prefix . " portfolioprojekt" : $title_prefix . " nytt portfolioprojekt";
+      break;
 }
 
 require_once("includes/layout/header.php");
 ?>
 
 <h1><?= $page_title ?></h1>
-<p>Här laddar vi ett formulär beroende på vad du vill redigera</p>
 
 <?php
+// Import the needed form
+switch($contentType) {
+   case "employment":
+      require("includes/editor/employment-form.php");
+      break;
+   case "education":
+      require("includes/editor/education-form.php");
+      break;
+   case "portfolio":
+      require("includes/editor/portfolio-form.php");
+      break;
+}
+
 require_once("includes/layout/footer.php");
