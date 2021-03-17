a6c3b467c099792a6508bfd5390298b93d04457f
Johannes Nilsson <johannes@nilsson.im>
2021-03-17 13:47:51 +0100

    Fetch and display education

--- admin.php
+++ admin.php
@@ -10,12 +10,35 @@ if(!isset($_SESSION["id"])) {
    header("Location: index.php?msg=Du måste logga in&type=error");
 }
 
+// Get all the required content
+$database = new Database();
+$db = $database->connect();
+//$employment = new Employment($db);
+$education = new Education($db);
+//$portfolio = new Portfolio($db);
+//$employment_items = $employment->getEmployments()["data"];
+//$portfolio_items = $portfolio->getPortfolios()["data"];
+$education_items = $education->getEducations()["data"];
+
 $page_title = "Adminpanel";
 require_once("includes/layout/header.php");
 ?>
 
 <h1>Hej <?= $_SESSION["name"] ?>! Du är nu inloggad.</h1>
 <p>Nedan kan du se lite information om det material du skapat.</p>
+
+<h2>Utbildningar</h2>
+<ul>
+<?php
+   foreach ($education_items as $item) { ?>
+      <li>
+         <a href="editor.php?contentType=education&id=<?= $item["id"] ?>">
+            <?= $item["title"] ?>
+         </a>
+      </li>
+<?php
+   } ?>
+</ul>
 
 <?php
 require_once("includes/layout/footer.php");
